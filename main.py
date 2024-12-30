78a63f679dfcd21d28be69e8f6a0e238d75e1280
mielesantos <mielesnts@gmail.com>
2024-12-29 22:04:01 -0300

    ajustes deploy

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+# from fastapi.staticfiles import StaticFiles
 
 from routers import api_router
 
 app = FastAPI(description='Agente financeiro')
-app.mount('/static', StaticFiles(directory='static'), name='static')
+# app.mount('/static', StaticFiles(directory='static'), name='static')
 
 
 app.include_router(api_router)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8001)
+    uvicorn.run(app, host='0.0.0.0', port=8080)
